aa80b85cc079605a81091e396153a9e54c7fc290
neutron98 <44017410+neutron98@users.noreply.github.com>
2020-07-03 20:48:54 -0400

    Create Solution.java

---
+++ 121. Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0){
+            return 0;
+        }
+        
+        int n = prices.length;
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        
+        for (int i = 0; i < n; i++){
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        
+        return maxProfit;
+    }
+}
