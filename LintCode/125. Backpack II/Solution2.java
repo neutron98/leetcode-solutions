06191757c9007684c9da81c6c9b9af0fb13b8174
neutron98 <44017410+neutron98@users.noreply.github.com>
2020-07-31 15:47:08 -0400

    Create Solution2.java

---
+++ LintCode/125. Backpack II/Solution2.java
@@ -0,0 +1,49 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @param V: Given n items with value V[i]
+     * @return: The maximum value
+     */
+    public int backPackII(int m, int[] A, int[] V) {
+        // write your code here
+        
+        // (1)if first {n-1} items can make up W, max value is V, then the first {n} items
+        // can also make up W, max value is V
+        
+        // (2) if first {n-1} items can make up W-A[n-1], max value is V-V[n-1]
+        // then plus last item can make W, max value is V 
+        
+        // f[i][w] = max value when using first {i} items to make up {w}
+        // -1 means cannot make
+        // f[i][w] = max{f[i-1][w], f[i-1][w-A[i-1]] + V[i-1]}  where w>=A[i-1] && f[i-1][w-A[i-1]] != -1}
+        // 重量不超，能拼出来
+        
+        // 3. init: f[0][0] = 0, f[0][1...M] = -1
+        // 4. answer: max{f[N][j]
+        
+        int n = A.length;
+        int[] f = new int[m+1];
+        
+        f[0] = 0;
+        for (int w = 1; w <= m; w++){
+            f[w] = -1;
+        }
+        
+        for (int i = 1; i <= n; i++){
+            for (int w = m; w >= A[i-1]; w--){
+                if (f[w-A[i-1]] != -1){
+                    f[w] = Math.max(f[w], f[w-A[i-1]] + V[i-1]);
+                }
+            }
+        }
+        
+        int res = 0;
+        for (int w = 0; w <= m; w++){
+            if (f[w] != -1){
+                res = Math.max(f[w], res);                
+            }
+        }
+        return res;
+    }
+}
