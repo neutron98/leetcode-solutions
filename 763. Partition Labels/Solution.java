56202bdd58f0552bb06c938fe1073e1ce9aa4649
neutron98 <44017410+neutron98@users.noreply.github.com>
2020-08-24 17:43:15 -0400

    Update Solution.java

--- 763. Partition Labels/Solution.java
+++ 763. Partition Labels/Solution.java
@@ -1,20 +1,18 @@
-// O(n)
-// 27
 class Solution {
     public List<Integer> partitionLabels(String S) {
-        int[] lastIndex = new int[27];
-        for (int i = 0; i < S.length(); i++){
-            int pos = S.charAt(i) - 'a' + 1;
-            lastIndex[pos] = i;
+        int[] lastIndex = new int[26];
+        int n = S.length();
+        for (int i = 0; i < n; i++){
+            int key = S.charAt(i) - 'a';
+            lastIndex[key] = i;
         }
         List<Integer> results = new ArrayList<>();
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < S.length(); i++){
-            int pos = S.charAt(i) - 'a' + 1;
-            end = Math.max(end, lastIndex[pos]);
-            if (i == end){
-                results.add(end - start + 1);
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++){
+            int key = S.charAt(i) - 'a';  // key for current character
+            end = Math.max(end, lastIndex[key]);   // extend the key
+            if (i == end){   // if i reaches the end of the partition
+                results.add(end - start + 1);  // add the length
                 start = end + 1;
             }
         }
