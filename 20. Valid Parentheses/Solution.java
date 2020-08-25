22223dedd9912706031a610ffba8dfd40fff2638
neutron98 <44017410+neutron98@users.noreply.github.com>
2020-08-25 14:51:57 -0400

    Create Solution.java

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() % 2 == 1){
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()){
+            if (map.containsKey(c)){  // right parenthesis
+                if (!stack.empty() && map.get(c) != stack.pop()){
+                    return false;
+                }
+            } else{ // left parenthesis
+                stack.push(c);
+            }
+        }
+        
+        return stack.empty();
+    }
+}
