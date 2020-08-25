6c3e9d0b4c59073dc4b1c3b2ce2dac615fce2226
neutron98 <44017410+neutron98@users.noreply.github.com>
2020-08-25 15:56:01 -0400

    Update Solution1.java

--- 67. Add Binary/Solution1.java
+++ 67. Add Binary/Solution1.java
@@ -1,4 +1,7 @@
 // Built in function
+// O(N+M)
+
+// 2 draw backs
 class Solution {
     public String addBinary(String a, String b) {
         return Integer.toBinaryString(Integer.parseInt(a, 2) + 
