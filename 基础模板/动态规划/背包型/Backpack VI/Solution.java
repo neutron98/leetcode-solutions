a5b6c51e030f4b967251a359ef0a3f86157f8d79
neutron98 <44017410+neutron98@users.noreply.github.com>
2021-01-30 13:23:25 -0500

    Update Solution.java

--- 基础模板/动态规划/背包型/Backpack VI/Solution.java
+++ 基础模板/动态规划/背包型/Backpack VI/Solution.java
@@ -5,20 +5,30 @@ public class Solution {
      * @return: An integer
      */
     public int backPackVI(int[] nums, int target) {
-        // write your code here
+        // last number put or not put
+        // 2. transition function 
+        // f[i] = f[i-m1] + f[i-m2] + f[i-m3]
+        
+        // 3. init and edges
+        // f[0] = 1
+        
+        // 4. order and answer 
+        // f[target]
+        
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
         
         int n = nums.length;
         int[] f = new int[target+1];
         f[0] = 1;
         for (int i = 1; i <= target; i++){
-            f[i] = 0;
-            for (int j = 0; j < n; j++){
-                if (i >= nums[j]){
-                    f[i] += f[i-nums[j]];
+            for (int num: nums){
+                if (i >= num){
+                    f[i] += f[i-num];
                 }
             }
         }
-        
         return f[target];
     }
 }
