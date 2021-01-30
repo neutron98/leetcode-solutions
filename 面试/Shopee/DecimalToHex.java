7c158ddba9184447a171b5d4a7ba1be36bc48ba4
neutron98 <44017410+neutron98@users.noreply.github.com>
2021-01-30 17:05:36 -0500

    Update DecimalToHex.java

--- 面试/Shopee/DecimalToHex.java
+++ 面试/Shopee/DecimalToHex.java
@@ -1,17 +1,17 @@
 class DecimalToHex{
 
-    public static void convert(int num){
-      int decimal = input.nextInt();
+    public static void convert(int decimal){
 
       String hex = "";
 
       while(decimal != 0){
         int hexValue = decimal%16;
         char hexDigit;
+          
         if (hexValue <= 9 && hexValue >= 0){
-          hexDigit = (char)(hexValue +'0');
+            hexDigit = (char)(hexValue +'0');
         } else{
-          hexDigit = (char)(hexValue -10 + 'A' );
+            hexDigit = (char)(hexValue -10 + 'A' );
         }
 
         hex = hexDigit + hex;
