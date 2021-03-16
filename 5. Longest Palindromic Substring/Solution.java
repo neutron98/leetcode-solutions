3c68f81b21978a61279371221dba97675facb830
neutron98 <44017410+neutron98@users.noreply.github.com>
2021-03-16 11:26:41 -0400

    Create Solution.java

---
+++ 5. Longest Palindromic Substring/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = Math.max(maxLen, len);
+                // aba   i = 1 len = 3
+                // start = 1 - 2/2 = 0, end = 1 + 3/2 = 1
+
+                // abba  i = 1  len = 4
+                // start = 1 - 3/2 = 0
+                // end = 1 + 4/2 = 3
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
